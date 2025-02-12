514fc3eea6f8b15548e23edf787c054f5e5041cf
malsebasmal <antoniomaldonadoestudio@gmail.com>
2025-02-12 14:57:59 -0500

    adding routes.js

--- src/assets/routes.js
+++ src/assets/routes.js
@@ -1,8 +1,47 @@
 export const routes = [
   {
-    id: 1,
     name: "San Bartolo",
-    difficult: "Media",
+    difficult: "media",
+    map: "https://www.google.com/maps/about/images/mymaps/mymaps-desktop-16x9.png"
+  },
+  {
+    name: "Leon Dormido",
+    difficult: "difícil",
+    map: "https://www.google.com/maps/about/images/mymaps/mymaps-desktop-16x9.png"
+  },
+  {
+    name: "Pentagonito",
+    difficult: "fácil",
+    map: "https://www.google.com/maps/about/images/mymaps/mymaps-desktop-16x9.png"
+  },
+  {
+    name: "Santa Rosa de Quives",
+    difficult: "media",
+    map: "https://www.google.com/maps/about/images/mymaps/mymaps-desktop-16x9.png"
+  },
+  {
+    name: "Canta",
+    difficult: "difícil",
+    map: "https://www.google.com/maps/about/images/mymaps/mymaps-desktop-16x9.png"
+  },
+  {
+    name: "Manchay",
+    difficult: "media",
+    map: "https://www.google.com/maps/about/images/mymaps/mymaps-desktop-16x9.png"
+  },
+  {
+    name: "Puente San Pedro",
+    difficult: "fácil",
+    map: "https://www.google.com/maps/about/images/mymaps/mymaps-desktop-16x9.png"
+  },
+  {
+    name: "Placita Cieneguilla",
+    difficult: "media",
+    map: "https://www.google.com/maps/about/images/mymaps/mymaps-desktop-16x9.png"
+  },
+  {
+    name: "Cto. de Playas de la Costa Verde",
+    difficult: "fácil",
     map: "https://www.google.com/maps/about/images/mymaps/mymaps-desktop-16x9.png"
   },
 ]
