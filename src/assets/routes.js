1700a53aea9bbac6c24a94860e36b821d7f66fbb
malsebasmal <antoniomaldonadoestudio@gmail.com>
2025-02-12 11:37:34 -0500

    delete react, create component badge and card route

---
+++ src/assets/routes.js
@@ -0,0 +1,8 @@
+export const routes = [
+  {
+    id: 1,
+    name: "San Bartolo",
+    difficult: "Media",
+    map: "https://www.google.com/maps/about/images/mymaps/mymaps-desktop-16x9.png"
+  },
+]
